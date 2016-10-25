717736594a09fef0c8076c493622591b475395e7
Erlo <erlo.conradie@gmail.com>
2016-10-25 23:26:26 +0200

    Used Divs, working. 1 Colour

--- java.js
+++ java.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
 
 
 	$('.btn').click(function(){
+		$('.data1').remove();
 		var $size = prompt("What size, sir?");
 		var hhh = (+$size + +1);
 		console.log(hhh+"JY SIEN");
@@ -23,22 +24,24 @@ $(document).ready(function(){
 		alert("You have chosen a sketch area of " + $size+ "x" +$size + ", which is " + $size*$size + " blocks!");
 		for (var x = 1; x < (+$size + +1); x++){
 			var id = 'gen'+x;
-			$('#table').append('<tr id=' +id+ '></tr>');
+			$('#area').append('<div class="data1" id=' +id+ '></div>');
+			$('.data1').css('height', $blocksize);
 				for(var y = 1; y < (+$size + +1); y++){
-					$('#gen'+x).append('<td class="data" height='+$blocksize+ ' width='+$blocksize+'></td>');
-
+					$('#gen'+x).append('<div class="data"></div>');
+					$('.data').css('height', $blocksize);
+					$('.data').css('width', $blocksize);
 				};
 				
 		};	
 
-	$(document).on('mouseenter', 'td', function(){
+	$(document).on('mouseenter', '.data', function(){
 		$(this).css('background-color', 'red');
 	});
-	$(document).on('mouseout', 'td', function(){
+	$(document).on('mouseout', '.data', function(){
 		$(this).fadeTo('blue');
 	});
 	$('#btn4').click(function(){
-		$('#table').empty();
+		$('#area').empty();
 	});
 	});
 });
