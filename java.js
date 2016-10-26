a1c35c8a31f484056aeea28a1d49d07b62b5f56f
Erlo <erlo.conradie@gmail.com>
2016-10-26 21:53:22 +0200

    Working as per challenge

--- java.js
+++ java.js
@@ -64,4 +64,30 @@ $(document).ready(function(){
 
 	});
 
+	$('#btn2').click(function(){
+		$('.data1').remove();
+		var $size = prompt("What size, sir?");
+		$('.data').css('opacity', '100%');
+		var hhh = (+$size + +1);
+		var $blocksize = (500/$size);
+		console.log($blocksize);
+		alert("You have chosen a sketch area of " + $size+ "x" +$size + ", which is " + $size*$size + " blocks!");
+		for (var x = 1; x < (+$size + +1); x++){
+			var id = 'gen'+x;
+			$('#area').append('<div class="data1" id=' +id+ '></div>');
+			$('.data1').css('height', $blocksize);
+				for(var y = 1; y < (+$size + +1); y++){
+					$('#gen'+x).append('<div class="data"></div>');
+					$('.data').css('height', $blocksize);
+					$('.data').css('width', $blocksize);
+				};
+		
+		};	
+		$(document).on('mouseenter', '.data', function(){
+			var $opa = $(this).css('opacity');
+			console.log($opa);
+					$(this).css('opacity', (+$opa - +0.1));
+		});
+
+	});
 });
